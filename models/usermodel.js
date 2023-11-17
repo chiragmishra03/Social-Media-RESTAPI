ba710598cb65ee102f70110cf80fe2d1dff1d1aa
CHIRAG MISHRA <chirag3102001@gmail.com>
2023-11-18 01:47:02 +0530

    created register and login route

---
+++ models/usermodel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Userschema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", Userschema);
---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const { Console } = require("console");
+const User = require("../models/usermodel");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedpassword,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+    !user && res.status(404).json("User not found");
+    const validpass = await bcrypt.compare(req.body.password, user.password);
+    !validpass && res.status(400).json("Wrong password");
+    res.status(200).json(user);
+  } catch (err) {
+    Console.log(err);
+  }
+});
+
+module.exports = router;
